e6e67b768ba5097942428c4e358097df34267c8d
Rasmus Kuurila <rasmuskuurila@outlook.com>
2023-01-12 12:30:59 +0200

    initial commit

---
+++ subly.js
@@ -0,0 +1,15 @@
+let totalPrice = 0;
+
+function addToTotal() {
+    // Get the value from the input field
+    let inputValue = document.getElementById("inputField").value;
+
+    // Convert the input value to a number
+    let inputNumber = parseFloat(inputValue);
+
+    // Add the input value to the total price
+    totalPrice += inputNumber;
+
+    // Update the total price displayed on the page
+    document.getElementById("totalPrice").innerHTML = totalPrice + "€";
+}
