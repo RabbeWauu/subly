bb155eff65355479700e3666e80c5bba01731494
Rasmus Kuurila <rasmuskuurila@outlook.com>
2023-01-16 19:09:29 +0200

    update javascript and html

--- subly.js
+++ subly.js
@@ -2,14 +2,17 @@ let totalPrice = 0;
 
 function addToTotal() {
     // Get the value from the input field
-    let inputValue = document.getElementById("inputField").value;
+    let subPrice = document.getElementById("subprice").value;
 
     // Convert the input value to a number
-    let inputNumber = parseFloat(inputValue);
+    let inputNumber = parseFloat(subPrice);
 
     // Add the input value to the total price
     totalPrice += inputNumber;
 
     // Update the total price displayed on the page
-    document.getElementById("totalPrice").innerHTML = totalPrice + "€";
+    document.getElementById("totalPrice").textContent = totalPrice + "€";
+}
+
+function createCard() {
 }
